515f2bb2148bd55282443ef628229673379324b3
Alex <burraum55@gmail.com>
2025-04-13 20:13:38 -0300

    ATUALIZACAO DO README
    atualizei o readme pra ficar mais objetivo e entendivel o intuito do projeto, tirei o persistance que nao estava usando e tirei umas coisas da main que estava dando bugs por causa do gmail unique.

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -7,17 +7,10 @@ import program.repositories.CustomizerFactory;
 public class Main {
     public static void main(String[] args) {
         EntityManager em = CustomizerFactory.getEntityManager();
-
         em.getTransaction().begin();
+
         UsuariosEntity cliente = new UsuariosEntity();
 
-        cliente.setNome("Alexandro ");
-        cliente.setTelefone("45999729416");
-        cliente.setEmail("contato2@gmail.com");
-        cliente.setSenha("506649");
-        cliente.setAdmin(true);
-
-        em.persist(cliente); // Persiste o objeto no banco de dados
         em.getTransaction().commit();
         em.close();
     }
