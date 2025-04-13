a39a7a895344b9d47854551ca98c0ce0d37ce719
Fabricio Quintana <fabriciorcq@gmail.com>
2025-04-13 19:49:56 -0300

    Update 4-13-2025
    -Adicionei um Menu de Visualizacao dos Agendamentos
    - Untested

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -1,8 +1,12 @@
 package program;
 
 import javax.persistence.EntityManager;
+
+import program.models.AgendamentoEntity;
 import program.models.UsuariosEntity;
 import program.repositories.CustomizerFactory;
+
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,12 +14,18 @@ public class Main {
 
         em.getTransaction().begin();
         UsuariosEntity cliente = new UsuariosEntity();
+        AgendamentoEntity agendamento1 = new AgendamentoEntity();
 
         cliente.setNome("Alexandro ");
         cliente.setTelefone("45999729416");
         cliente.setEmail("contato2@gmail.com");
         cliente.setSenha("506649");
         cliente.setAdmin(true);
+
+        agendamento1.setCliente(cliente);
+        agendamento1.setDescricao("Teste de agendamento...");
+        agendamento1.setDataHora(LocalDateTime.of(2025, 04, 28, 14, 33));
+        agendamento1.setStatus("Marcado");
 
         em.persist(cliente); // Persiste o objeto no banco de dados
         em.getTransaction().commit();
