d0414123ead39fd982fd88f1f85d95725e2c44d9
João Pedro Reis Felini <joaoreisfelini@gmail.com>
2025-04-29 21:08:53 -0300

    ADD: implementado BCrypt (hash de senhas)

--- src/main/java/org/example/functions/MenuLoginFunction.java
+++ src/main/java/org/example/functions/MenuLoginFunction.java
@@ -4,13 +4,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import java.util.Scanner;
 import org.example.models.UsuariosEntity;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class MenuLoginFunction {
     public static void Menulogin(EntityManager em, Scanner sc) {
         System.out.println("Menu Login\nDigite o e-mail: ");
         String email = sc.nextLine();
-        System.out.println("Digite a senha: ");
-        String senha = sc.nextLine();
 
         String prefixo = "SELECT u FROM UsuariosEntity u WHERE u.email = :email";
         TypedQuery<UsuariosEntity> query = em.createQuery(prefixo, UsuariosEntity.class);
@@ -21,16 +20,23 @@ public class MenuLoginFunction {
             usuario = query.getSingleResult();
         } catch (Exception e) {
             System.out.println("E-mail não encontrado. Pressione ENTER para retornar ao menu principal.");
-            sc.next();
+            sc.nextLine();
             return;
         }
 
-        if (usuario.getSenha().equals(senha)) {
+        if (usuario.isAdmin() == false){
+            System.out.println("Usuário não tem permissões administrativas. Pressione ENTER para retornar ao menu principal");
+            sc.nextLine();
+            return;
+        }
+
+        System.out.println("Digite a senha: ");
+        if (BCrypt.checkpw(sc.nextLine(), usuario.getSenha())) {
             System.out.println("Acesso permitido!\n\n");
             MenuAdmin.menuAdministrador();
         } else {
             System.out.println("Senha incorreta. Pressione ENTER para retornar ao menu principal.");
-            sc.next();
+            sc.nextLine();
         }
     }
 }
