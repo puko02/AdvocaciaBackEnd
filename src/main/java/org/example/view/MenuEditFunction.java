68f0105439271d1bdd837cbeb5a76c544e27cc60
João Pedro Reis Felini <joaoreisfelini@gmail.com>
2025-06-02 19:55:27 -0300

    add: Adicionado possibilidade de editar isActive e HTML para o email de confirmação

--- src/main/java/org/example/view/MenuEditFunction.java
+++ src/main/java/org/example/view/MenuEditFunction.java
@@ -24,21 +24,22 @@ public class MenuEditFunction {
 
         UsuariosEntity usuario = usuarioOpt.get();
         int option = 1;
-
         while (option != 0) {
             System.out.println("\nDados atuais do usuário:");
             System.out.println("    Nome: " + usuario.getNome());
             System.out.println("    E-mail: " + usuario.getEmail());
+            System.out.println("    O e-mail passou pela verificação? " + (usuario.isActive() ? "Sim" : "Não"));
             System.out.println("    Telefone: " + usuario.getTelefone());
             System.out.println("    Senha (com Hash): " + usuario.getSenha());
-            System.out.println("    Usuário é admin? " + usuario.isAdmin());
+            System.out.println("    Usuário é admin? " + (usuario.isAdmin() ? "Sim" : "Não"));
 
             System.out.println("\nO que deseja editar?");
             System.out.println("    1. Nome");
             System.out.println("    2. Número de telefone");
             System.out.println("    3. Senha");
             System.out.println("    4. Alterar para admin/usuário comum");
-            System.out.println("    5. Excluir cliente");
+            System.out.println("    5. Alterar usuário para ativo/inativo (Verificação de e-mail)");
+            System.out.println("    6. Excluir cliente");
             System.out.println("    0. Retornar ao menu admin");
 
             option = sc.nextInt();
@@ -58,6 +59,9 @@ public class MenuEditFunction {
                     usuarioService.editarIsAdmin(usuario);
                 break;
                 case 5:
+                    usuarioService.editarIsActive(usuario);
+                    break;
+                case 6:
                     System.out.println("Tem certeza que deseja excluir este usuário? (s/n)");
                     String confirm = sc.nextLine();
                     if (confirm.equalsIgnoreCase("s")) {
